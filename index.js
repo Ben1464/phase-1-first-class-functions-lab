4ed3aa77794c434511fa5f4af1aac90886ec4900
Benson Mwangi <kimaniben@gmail.com>
2023-10-01 04:17:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const drivers=['James','Alex','Jane','Benson'];
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+function createFareMultiplier(x){
+    return (y) => x * y;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function fareDoubler(x){
+    return x * 2;
+}
+
+function fareTripler(x){
+    return x *3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, functionX){
+    return functionX(arrayOfDrivers)
+}
